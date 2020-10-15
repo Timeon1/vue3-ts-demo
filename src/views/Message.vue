a6ae63149b1b6c113dfb887deb2be3550e9284aa
Timeon1 <42144027+Timeon1@users.noreply.github.com>
2020-10-15 11:01:51 +0800

    引入Vant

--- src/views/Message.vue
+++ src/views/Message.vue
@@ -3,13 +3,17 @@
     <h1>This is an Message page</h1>
     <h1>{{count}}</h1>
     <button @click="plus()">plus</button>
+    <van-button type="primary">啊萨达萨达所</van-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent,reactive,toRefs } from 'vue'
-
+import { Button } from 'vant';
 export default defineComponent({
+  components:{
+    // [Button.name]: Button,
+  },
   setup() {
     const state:{
       count: number
